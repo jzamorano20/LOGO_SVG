3d2fbc1f07228cf5ef242766a7943e41f65481ae
jzamorano20 <126306433+jzamorano20@users.noreply.github.com>
2023-04-16 16:48:51 -0400

    shapes fixed

--- index.js
+++ index.js
@@ -1,21 +1,56 @@
-class shape (
-    constructor (type){
-        this.type = type;
-        this.color = 'teal'
+const inquirer = require('inquirer');
+const fs = require('fs');
+const { Triangle, Square, Circle } = require('./lib/shapes');
+const { throwError } = require('rxjs');
+
+const questions = [{
+    //the input.length will make they only 3 characters or less will appear
+    name: 'text',
+    message: 'Enter three characters:',
+    validate: (input) => input.length <= 3,
+},
+{
+    type: 'input',
+    name: 'textColor',
+    message: 'Enter a color keyword (or hexadecimal number)',
+
+},
+{
+    type: 'list',
+    name: 'shape',
+    message: 'Pick a shape from the list',
+    choices: ['Triangle', 'Square', 'Circle'],
+},
+{
+    type: 'input',
+    name: 'shapeColor',
+    message: 'Enter a color keyword (or hexadecimal number',
+},
+];
+
+inquirer.prompt(questions).then((answers) => {
+    var shape;
+    switch (answers.shape) {
+        case 'Triangle':
+            shape = new Triangle(answers.shapeColor);
+            break;
+        case 'Square':
+            shape = new Square(answers.shapeColor);
+            break;
+        case 'Circle':
+            shape = new Circle(answers.shapeColor);
+            break;
     }
-    setColor(color); 
-    {this.color = color;}
+    // this should create the SVG img with the shape, color, text thanks to render
+    const svg = `<svg width="200" height="250" version="1.1" xmlns="http://www.w3.org/2000/svg">
+${shape.render()}
+<text x="100" y="125" font-size="60" text-anchor="middle" fill="${answers.textColor}"> ${answers.text}</text>
+</svg>`;
 
-);
+    fs.writeFile('logo.svg', svg, (error) => {
+        if (error) { throw error };
+        console.log("Congratulations, you have Generated a logo.svg!")
+    });
 
-class Circle extends shape {
-    constructor(type) {
-        super(type);
-    };
-};
 
-class Triangle extends shape {
-    constructor(type) {
-        super(type);
-    }
-}
+});
